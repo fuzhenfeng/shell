5a4c529964f54caf57bd97414c79eb6292c594cf
fuzhenfeng <caomangshidai@163.com>
2018-12-14 02:50:01 +0000

    Update spring.sh
    循环检测进程，这样更准确

--- spring.sh
+++ spring.sh
@@ -3,6 +3,10 @@ source /etc/profile
 
 COMMAND="$1"
 APP_NAME=$2
+APP_PORT=$3
+FILE=$4
+JAR_NAME=""
+
 BASE_PATH=$(cd `dirname $0`; pwd)
 LOCAL_IP=`ifconfig eth0 |grep "inet addr"| cut -f 2 -d ":"|cut -f 1 -d " "`
 
@@ -18,8 +22,8 @@ init_jms_port(){
 }
 
 is_exist(){
-  NAME=${APP_NAME/.jar/}
-  PID=`ps -ef|grep ${NAME}|grep -v grep|grep -v "spring.sh"|awk '{print $2}'`
+  JAR_NAME=${APP_NAME}".jar"
+  PID=`ps -ef|grep ${JAR_NAME}|grep -v grep|grep -v "spring.sh"|awk '{print $2}'`
   if [ -z "${PID}" ]; then
     return 1
   else
@@ -30,12 +34,12 @@ is_exist(){
 start(){
   is_exist
   if [ $? -eq "0" ]; then
-    echo "${APP_NAME} is already running. Pid is ${PID}"
+    echo "${JAR_NAME} is already running. Pid is ${PID}"
   else
-    chmod 777 ${BASE_PATH}/$APP_NAME
-    echo "${APP_NAME} start..."
+    chmod 777 ${BASE_PATH}/${JAR_NAME}
+    echo "${JAR_NAME} start..."
     init_jms_port
-    JAVA_OPTS="-Dname=$APP_NAME \
+    JAVA_OPTS="-Dname=${APP_NAME} \
     -Xms512m -Xmx512m -Xmn128m -Xss256k \
     -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m \
     -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=dump \
@@ -46,55 +50,63 @@ start(){
     -Dcom.sun.management.jmxremote.ssl=false \
     -Dcom.sun.management.jmxremote.port=${JMX_PORT} \
     -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT}"
-    nohup java ${JAVA_OPTS} -jar ${BASE_PATH}/$APP_NAME >> nohup.out 2>&1 &
+    nohup java ${JAVA_OPTS} -jar ${BASE_PATH}/${JAR_NAME} --server.port=${APP_PORT} >> nohup.out 2>&1 &
   fi
 }
  
 stop(){
   is_exist
   if [ $? -eq "0" ]; then
-    	echo "${APP_NAME} stop..."
-	STOP_MSG=`curl -X POST http://${LOCAL_IP}:10002/eden/actuator/shutdown`
+    	echo "${JAR_NAME} stop..."
+	STOP_MSG=`curl -X POST http://${LOCAL_IP}:${APP_PORT}/${APP_NAME}/actuator/shutdown -s`
 	echo ${STOP_MSG}
+	while true; do
+		is_exist
+		if [ $? -eq "0" ]; then
+			echo "${APP_NAME} stop..."
+			sleep 1s
+		else
+			break
+		fi
+	done
   else
-    	echo "${APP_NAME} is not running"
+    	echo "${JAR_NAME} is not running"
   fi
 }
  
 status(){
   is_exist
   if [ $? -eq "0" ]; then
-    echo "${APP_NAME} is running. Pid is ${PID}"
+    echo "${JAR_NAME} is running. Pid is ${PID}"
   else
-    echo "${APP_NAME} is NOT running."
+    echo "${JAR_NAME} is NOT running."
   fi
 }
 
 move_old(){
-    echo "${APP_NAME} backup jar..."
+    echo "${JAR_NAME} backup jar..."
     DATE=`date -d today +"%Y%m%d%H%M%S"`
-    mv ${BASE_PATH}/${APP_NAME} ${BASE_PATH}/backup/${DATE}"-"${APP_NAME}
+    mv ${BASE_PATH}/${JAR_NAME} ${BASE_PATH}/backup/${DATE}"-"${JAR_NAME}
     if [ $? -eq "0" ]; then
-      echo "${APP_NAME} backup ok"
+      echo "${JAR_NAME} backup ok"
     else
-      echo "${APP_NAME} backup fail"
+      echo "${JAR_NAME} backup fail"
     fi
 }
 
 move_new(){
-    echo "${APP_NAME} move jar..."
-    mv ${BASE_PATH}/temp/target/${APP_NAME} ${BASE_PATH}/${APP_NAME}
+    echo "${JAR_NAME} move jar..."
+    mv ${FILE} ${BASE_PATH}/${JAR_NAME}
     if [ $? -eq "0" ]; then
-      echo "${APP_NAME} move jar ok"
+      echo "${JAR_NAME} move jar ok"
     else
-      echo "${APP_NAME} move jar fail"
+      echo "${JAR_NAME} move jar fail"
       exit 1
     fi
 }
  
 restart(){
   stop
-  sleep 2s
   move_old
   move_new
   start
